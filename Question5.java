23aa5a4e2e6351c8a37b5d58af6ae5531ebfd651
macfreze <93721199+macfreze@users.noreply.github.com>
2022-04-25 00:06:47 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,35 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Input a number: ");
+    int num = in.nextInt();
+
+    ArrayList<Integer> numbers = new ArrayList();
+
+    for(int i = 0; i < num ; i++){
+      System.out.print("Input another number: ");
+      int newNum = in.nextInt();
+
+      numbers.add(newNum);
+    }
+
+      
+    int mode = numbers.get(0);
+    int maxCount = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        int number = numbers.get(i);
+        int count = 1;
+        for (int j = 0; j < numbers.size(); j++) {
+            if (numbers.get(j) == number)
+                count++;
+            if (count > maxCount) {
+                mode = number;
+                maxCount = count;
+            }
+        }
+    }
+
+    System.out.print("Mode of your numbers is: " + mode);
     
   }
 }
