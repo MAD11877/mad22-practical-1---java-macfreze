40c79eaf1ecffd4a63d776a9a5aeec170db479b5
macfreze <93721199+macfreze@users.noreply.github.com>
2022-04-25 00:24:12 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,12 +11,12 @@ public class Question3
      */
     Scanner in = new Scanner(System.in);
 
-    System.out.print("Input a number: ");
+    
     int number = in.nextInt();
 
     int newNumber = number * number;
 
-    System.out.print("The square of your number is: " + newNumber);
+    System.out.print(newNumber);
     
   }
 }
