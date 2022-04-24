b210ae7d0492b64919691bd1feb20062f6db2522
macfreze <93721199+macfreze@users.noreply.github.com>
2022-04-24 18:30:12 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,14 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Input a number: ");
+    int number = in.nextInt();
+
+    int newNumber = number * number;
+
+    System.out.print("The square of your number is: " + newNumber);
     
   }
 }
